afc1fa4898d85112ed0e6601c9547d0e19a2e275
TommyYuSky <yutianlong21@gmail.com>
2015-10-08 19:14:25 -0400

    cleanup

--- BuzzGui/GlobalInput.js
+++
@@ -1,7 +0,0 @@
-#Traffic
-192.168.123.1 192.168.123.2 
-#Enforcement
-Mktg 7000 Fw
-Fw all LB
-LB all CRM
-#Customize
--- BuzzGui/PGAinput.js
+++
@@ -1,6 +0,0 @@
-#Traffic
-192.168.123.1 192.168.123.2 
-#Enforcement
-Bro HTTP_GET DPI
-Bro HTTP_POST HeaderCheck
-#Customize
